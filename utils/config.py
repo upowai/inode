d97b487c421e662a0028b68ef01a7110b2f90737
gprethesh <georgeprethesh@georges-MacBook-Air.local>
2024-03-21 14:29:12 +0530

    config update

--- utils/config.py
+++ utils/config.py
@@ -14,17 +14,19 @@ class Env:
 env = Env()
 
 # Configuration settings
-FETCH_VALIDATORS = "http://127.0.0.1:3006/get_validators_info?inode=DhWyMUj2pna2UYbvrqULyLf6dEo2MNzPHA7Uh4kBrJGFY"
-CORE_URL = "http://127.0.0.1:3006"
-INODE_IP = "127.0.0.1"
+FETCH_VALIDATORS = (
+    "get_validators_info?inode=DbHgJVR7gDdTaxx7v3kX3GsxfJnTXy9zouSuzXE1TUWmB"
+)
+CORE_URL = "https://api.upow.ai"
+INODE_IP = "0.0.0.0"
 INODE_PORT = 65432
 INODE_BUFFER = 1024
 
 # Other configurations
-INODE_WALLET_ADDRESS = "DhWyMUj2pna2UYbvrqULyLf6dEo2MNzPHA7Uh4kBrJGFY"
+INODE_WALLET_ADDRESS = "DbHgJVR7gDdTaxx7v3kX3GsxfJnTXy9zouSuzXE1TUWmB"
 INODE_REWARD_WALLET_ADDRESS = "Djxhpx8ogGwpfe1tHxuBLVuxXZEhrS7spstDuXUugJ32i"
 PRIVATEKEY = env.PRIVATEKEY
-API_URL = "http://127.0.0.1:3006"
+API_URL = "https://api.upow.ai"
 FAST_API_URL = "0.0.0.0"
 FAST_API_PORT = 8000
-TRACK = 500
+TRACK = 15616
